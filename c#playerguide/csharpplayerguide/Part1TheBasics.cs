5aa0ab08c655b9e2447875f4a214a919ef50be52
Zodiark619 <seonghyejin33@gmail.com>
2022-08-07 19:13:11 +0700

    bababa

---
+++ c#playerguide/csharpplayerguide/Part1TheBasics.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpplayerguide
+{
+    internal class Part1TheBasics
+    {
+
+
+
+        public static void TheDefenseOfConsolas_15()
+        {
+            Console.Write("Target Row? ");
+            int row = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Target Column? ");
+            int column = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Deploy to:");
+            Console.WriteLine($"({row}, {column-1})");
+            Console.WriteLine($"({row-1}, {column })");
+
+            Console.WriteLine($"({row}, {column + 1})");
+            Console.WriteLine($"({row+1}, {column })");
+
+
+        }
+    }
+}
