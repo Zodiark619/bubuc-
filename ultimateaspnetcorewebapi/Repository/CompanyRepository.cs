43befb7198ec4c0f4ace1985f36a8290f2f667fa
Zodiark619 <seonghyejin33@gmail.com>
2022-08-18 07:05:06 +0700

    uykukyu

---
+++ ultimateaspnetcorewebapi/Repository/CompanyRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contracts;
+using Entities;
+
+namespace Repository
+{
+    public class CompanyRepository : RepositoryBase<CompanyRepository>, ICompanyRepository
+    {
+        public CompanyRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+        }
+    }
+}
