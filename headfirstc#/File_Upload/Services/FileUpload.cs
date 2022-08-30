ac09741b449fcd359bfee6e6a7ae3644ec9399fb
Zodiark619 <seonghyejin33@gmail.com>
2022-08-30 20:48:34 +0700

    asdfew

---
+++ headfirstc#/File_Upload/Services/FileUpload.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace File_Upload.Services
+{
+
+    public interface IFileUpload
+    {
+        Task UploadFile(IBrowserFile file);
+    }
+    public class FileUpload : IFileUpload
+    {
+        private IWebHostEnvironment _webHostEnvironment;
+        private readonly ILogger<FileUpload> _logger;
+
+        public FileUpload(IWebHostEnvironment webHostEnvironment, ILogger<FileUpload> logger)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _logger = logger;
+        }
+
+        public async Task UploadFile(IBrowserFile file)
+        {
+            if(file is not null)
+            {
+                try
+                {
+                    var uploadPath=Path.Combine(_webHostEnvironment.WebRootPath,"uploads",file.Name);
+                    using (var stream =  file.OpenReadStream())
+                    {
+                        var filestream= File.Create(uploadPath);
+                        await stream.CopyToAsync(filestream);
+                        filestream.Close();
+                    }
+                }
+                catch(Exception ex)
+                {
+                    _logger.LogError(ex.ToString());
+                }
+            }
+        }
+    }
+}
